b2c1ffc222ca68f0c24c65c36070cbc3fff1d235
Martin Wilke <miwi@FreeBSD.org>
2018-06-21 00:48:48 +0800

    - Update info

--- post_install.sh
+++ post_install.sh
@@ -3,5 +3,9 @@
 # Enable the service
 sysrc -f /etc/rc.conf xmrig_enable="YES"
 
+echo "Please follow the Installation Instructions at https://github.com/xmrig/xmrig"
+echo "The config can be found at: /usr/local/etc/xmrig/config.json"
+
+
 # Start the service
 service xmrig start 2>/dev/null
