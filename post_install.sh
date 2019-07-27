d8cc6d4d62acf1b3aa53ca452fa85b0c50e030cd
Martin Wilke <miwi@ixsystems.com>
2019-07-28 00:19:14 +0800

    NAS-102660

--- post_install.sh
+++ post_install.sh
@@ -3,8 +3,8 @@
 # Enable the service
 sysrc -f /etc/rc.conf xmrig_enable="YES"
 
-echo "Please follow the Installation Instructions at https://github.com/xmrig/xmrig"
-echo "The config can be found at: /usr/local/etc/xmrig/config.json"
+echo "Please follow the Installation Instructions at https://github.com/xmrig/xmrig" > /root/PLUGIN_INFO
+echo "The config can be found at: /usr/local/etc/xmrig/config.json" >> /root/PLUGIN_INFO
 
 
 # Start the service
